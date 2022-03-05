ffc45be8db25160d3ecf46bf3e1ab538f7fc7a57
swanovna <to.tatiana.lebedeva@gmail.com>
2022-03-05 20:03:23 +1300

    api connected

--- src/app.js
+++ src/app.js
@@ -1,1 +1,53 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
+}
 
+function displayTemperature(response) {
+  console.log(response.data);
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+
+  let conditionsElement = document.querySelector("#conditions");
+  conditionsElement.innerHTML = response.data.weather[0].description;
+
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+
+  let iconElement = document.querySelector("#main-icon");
+  iconElement.innerHTML = response.data.weather[0].icon;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+
+  let temperatureElement = document.querySelector("#current-temp");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "342444b367d13cc01160f5e0ec42822b";
+let city = "Auckland";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
